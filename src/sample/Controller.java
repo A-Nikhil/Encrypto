fd4f0e44a2b7fac3aaa82e711ae5e50b79876373
theGeekGod <nikhilanand1006@gmail.com>
2018-12-14 23:59:25 +0530

    Stage 1 complete - Update 1.0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,3 +1,7 @@
+/*
+*
+ */
+
 package sample;
 
 import javafx.event.ActionEvent;
@@ -5,12 +9,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.DriverManager;
 
 public class Controller {
 
@@ -67,7 +76,10 @@ public class Controller {
         String password = pass.getText();
         String passwordAgain = passAgain.getText();
         String Email = email.getText();
-        if(!password.equalsIgnoreCase(passwordAgain)) {
+        if (Name == null || username == null || password == null || passwordAgain == null || Email == null) {
+            statusBarSgnup.setText("Status: One of the fields is null, FILL IT");
+            statusBarSgnup.setTextFill(Paint.valueOf("#EC0A0A"));
+        } else if(!password.equalsIgnoreCase(passwordAgain)) {
             statusBarSgnup.setText("Status: Passwords don't match, type them again");
             pass.clear();
             passAgain.clear();
@@ -77,6 +89,26 @@ public class Controller {
             email.clear();
             statusBarSgnup.setTextFill(Paint.valueOf("#EC0A0A"));
         } else {
+            try {
+                Connection c = DriverManager.getConnection("jdbc:sqlite:D:\\MyProjects\\Encrypto\\db\\Encrypto.db");
+
+                // Finding a unique user id
+                Statement stmt = c.createStatement();
+                ResultSet FAQ = stmt.executeQuery("SELECT MAX(USERID) FROM USERS");
+                int userID = FAQ.getInt(1) + 1;
+                stmt.close();
+
+                PreparedStatement pstmt = c.prepareStatement("INSERT INTO USERS(USERID, USERNAME, PASSWORD, NAME, EMAIL) VALUES (?, ?, ?, ?, ?);");
+                pstmt.setInt(1, userID);
+                pstmt.setString(2, username);
+                pstmt.setString(3, password);
+                pstmt.setString(4, Name);
+                pstmt.setString(5, Email);
+                pstmt.executeUpdate();
+                pstmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             System.out.println("Success");
             statusBarSgnup.setText("Status: Login Successful, Opening your page shortly");
             statusBarSgnup.setTextFill(Paint.valueOf("#43D61F"));
@@ -88,16 +120,32 @@ public class Controller {
     public void loginCreds(ActionEvent event) {
         String username = userName.getText();
         String password = passWord.getText();
-        if(username.equals("ABC") && password.equals("123")) {
-            System.out.println("Success");
-            statusBar.setText("Status: Login Successful, Opening your page shortly");
-            statusBar.setTextFill(Paint.valueOf("#43D61F"));
-        } else {
-            System.out.println("Failure");
-            statusBar.setText("Status: Login Unsuccessful, Try again");
-            userName.clear();
-            passWord.clear();
-            statusBar.setTextFill(Paint.valueOf("#EC0A0A"));
+        try {
+            boolean found = false;
+            Connection c = DriverManager.getConnection("jdbc:sqlite:D:\\MyProjects\\Encrypto\\db\\Encrypto.db");
+            Statement stmt = c.createStatement();
+            ResultSet login = stmt.executeQuery("SELECT USERNAME, PASSWORD FROM USERS");
+            while (login.next()) {
+                String chk1 = login.getString(1);
+                String chk2 = login.getString(2);
+                if(username.equalsIgnoreCase(chk1) && password.equalsIgnoreCase(chk2)) {
+                    found = true;
+                    break;
+                }
+            }
+            if(found) {
+                System.out.println("Success");
+                statusBar.setText("Status: Login Successful, Opening your page shortly");
+                statusBar.setTextFill(Paint.valueOf("#43D61F"));
+            } else {
+                System.out.println("Failure");
+                statusBar.setText("Status: Login Unsuccessful, Try again");
+                userName.clear();
+                passWord.clear();
+                statusBar.setTextFill(Paint.valueOf("#EC0A0A"));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
